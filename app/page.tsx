24edfff5c7527bc23f6807f5d8bbb855d2edd2bc
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-08-13 16:11:56 +0300

    start catalogue

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import { Hero } from '@/components'
-import Image from 'next/image'
+import { CustomFilter, Hero, SearchBar } from '@/components';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-    <Hero />
+      <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h2 className="text-4xl font-extrabold">Car Catalogue</h2>
+          <p className="">Explore the cars you might like</p>
+        </div>
+        <div className="homefilters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
